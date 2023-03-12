6cfd5329229d918eafad150cf19ddf8eae26cdd2
dimilidi <dimitrovalidia@yahoo.com>
2023-03-13 00:17:42 +0100

    .

--- src/components/Calculator/Buttons/ResetKey.js
+++ src/components/Calculator/Buttons/ResetKey.js
@@ -4,6 +4,7 @@ import {ACTIONS} from '../../../constants/constants';
 
 export function ResetKey({dispatch, symbol}) {
   return (
+    
     <button onClick={() => dispatch({type: ACTIONS.CLEAR})}>{symbol}</button>
   )
 }
